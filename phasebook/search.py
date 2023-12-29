22f28205bf7280b7275820b0dc7f5c714688c1ec
Yuu-Rei <rhey.depz02@gmail.com>
2023-12-30 03:13:20 +0800

    modifed search function to better fit sorting specification

--- phasebook/search.py
+++ phasebook/search.py
@@ -23,53 +23,48 @@ def search_users(args):
     Returns:
         a list of users that match the search parameters
     """
-    # Extract search parameters from the args dictionary
     search_id = args.get("id")
     search_name = args.get("name")
     search_age = args.get("age")
     search_occupation = args.get("occupation")
 
-    # Initialize an empty list to store the results
     results = []
 
-    # If the id is provided, include the user with that id in the results
     if search_id:
         user_with_id = next((user for user in USERS if user["id"] == search_id), None)
         if user_with_id:
             results.append(user_with_id)
 
-    # Filter users based on name (case-insensitive partial match)
     if search_name:
         search_name_lower = search_name.lower()
         results.extend([user for user in USERS if search_name_lower in user["name"].lower()])
 
-    # Filter users based on age (within the range of age - 1 to age + 1)
     if search_age:
         age_lower = int(search_age) - 1
         age_upper = int(search_age) + 1
         results.extend([user for user in USERS if age_lower <= user["age"] <= age_upper])
 
-    # Filter users based on occupation (case-insensitive partial match)
     if search_occupation:
         search_occupation_lower = search_occupation.lower()
         results.extend([user for user in USERS if search_occupation_lower in user["occupation"].lower()])
 
-    # If no matching results are found, return all users
+    # If no matching search, return all users
     if not results:
         return USERS
 
     # Remove duplicates from the results
     unique_results = [dict(t) for t in {tuple(d.items()) for d in results}]
 
-    # Sort the results based on the priority of matching criteria
     unique_results.sort(
-        key=lambda user: (
-            search_id and user.get("id") == search_id,
-            search_name and search_name_lower in user.get("name", "").lower(),
-            search_age and (age_lower <= user.get("age", 0) <= age_upper if user.get("age") else False),
-            search_occupation and search_occupation_lower in user.get("occupation", "").lower(),
-        ),
-        reverse=True,
-    )
-    
+    key=lambda user: (
+        -(search_id == user.get("id")) if search_id else 0,
+        -(search_name_lower in user.get("name", "").lower()) if search_name else 0,
+        -(search_age and (age_lower <= user.get("age", 0) <= age_upper) if user.get("age") else False) if search_age else 0,
+        -(search_occupation_lower in user.get("occupation", "").lower()) if search_occupation else 0,
+        user.get("id", ""),  # Sorting by id in ascending order
+        user.get("name", "").lower(),  # Sorting by name in ascending order
+        -user.get("age", 0),  # Sorting by age in descending order
+        user.get("occupation", "").lower(),  # Sorting by occupation in ascending order
+    ),
+)
     return unique_results
